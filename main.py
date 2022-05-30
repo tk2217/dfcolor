31146a3393750c96a58e71f59a71110fd70fa34d
tk2217 <git@tk2217.com>
2022-05-29 20:37:16 -0500

    more

--- main.py
+++ main.py
@@ -23,20 +23,21 @@ def horrible_bungee_format_color(color):
         out += f"&{c}"
     return out
 
-named = loadProps("named.txt")
-data = loadProps("data.txt")
+if __name__ == "__name__":
+    named = loadProps("named.txt")
+    data = loadProps("data.txt")
 
-for k, v in data.items():
-    color = ""
-    if v.startswith("NamedTextColor."):
-        color = '%06x' % int(named[v.replace("NamedTextColor.", "")], 16)
-    else:
-        pts = v.split(", ")
-        color = "%02x%02x%02x" % (int(pts[0]), int(pts[1]), int(pts[2]))
-    # print(f"<#{color}>{k}: #{color}")
-    print(f"""
-        <span class=\"mc-font cls\" style=\"color: #{color};\">
-            <span class="sel">{k}</span>: <span class="sel">#{color}</span> - <span class="sel">{horrible_bungee_format_color(color)}</span>
-        </span>
-        <br/>
-    """)
+    for k, v in data.items():
+        color = ""
+        if v.startswith("NamedTextColor."):
+            color = '%06x' % int(named[v.replace("NamedTextColor.", "")], 16)
+        else:
+            pts = v.split(", ")
+            color = "%02x%02x%02x" % (int(pts[0]), int(pts[1]), int(pts[2]))
+        # print(f"<#{color}>{k}: #{color}")
+        print(f"""
+            <span class=\"mc-font cls\" style=\"color: #{color};\">
+                <span class="sel">{k}</span>: <span class="sel">#{color}</span> - <span class="sel">{horrible_bungee_format_color(color)}</span>
+            </span>
+            <br/>
+        """)
---
+++ makeenum.py
@@ -0,0 +1,20 @@
+from main import loadProps
+
+named = loadProps("named.txt")
+data = loadProps("data.txt")
+
+print("class DFColor {")
+for k, v in data.items():
+    color = 0
+    if v.startswith("NamedTextColor."):
+        color = int(named[v.replace("NamedTextColor.", "")], 16)
+    else:
+        pts = v.split(", ")
+        r = int(pts[0]) << 16
+        g = int(pts[1]) << 8
+        b = int(pts[2])
+        color = r + g + b
+    
+    print(f"    public static final int {k} = {hex(color)};")
+
+print("}")
